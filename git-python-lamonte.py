12575809545a643729c1556cf5b84ae538169fd5
LamonteKristufJeruz <kristuflamonte29@gmail.com>
2023-03-09 17:38:52 +0800

    My Educational Background added

--- git-python-lamonte.py
+++ git-python-lamonte.py
@@ -1,3 +1,7 @@
 print("Kristuf Jeruz J. Lamonte")
 print("Pangi, Gasan, Marinduque")
 print("August 12, 2002")
+print("")
+print("Marinduque State College")
+print("Bachelor of Science in Information Technology")
+print("BSIT - 2C")
